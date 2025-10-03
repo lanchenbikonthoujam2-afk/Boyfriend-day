2fbc3ff055de977dc672c8f7ff575f67630c2eaa
lanchenbikonthoujam2-afk <lanchenbikonthoujam2@gmail.com>
2025-10-03 15:32:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// This is the file where you will add interactivity!
+
+document.addEventListener('DOMContentLoaded', () => {
+    // A simple test to make sure the script is linked
+    console.log('Script file is running!');
+
+    // Example of a function that runs when the page loads:
+    alert('A little message to surprise him when he opens the page!');
+});
